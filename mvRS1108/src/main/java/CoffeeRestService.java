2d8f0f97ee3d77420b36ebd69a9739dbefdbf9e3
student <student@DESKTOP-4ARRLB2>
2022-11-09 13:36:07 +0800

    Coffee Update&Delete

--- mvRS1108/src/main/java/CoffeeRestService.java
+++ mvRS1108/src/main/java/CoffeeRestService.java
@@ -1,9 +1,12 @@
+import java.sql.SQLException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import dao.CoffeeDao;
 import model.Coffee;
 
 @Path("/coffee")
@@ -13,9 +16,45 @@ public class CoffeeRestService {
 	@Path("/post")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String postCoffee(Coffee cf) {
+	public String postCoffee(Coffee cf) throws SQLException {
 		String result = "Record entered: "+ cf.toString();
         System.out.println(result);
-		return "OK";
+        int r = new CoffeeDao().InsertCoffee(cf);
+        if(r > 0)
+        	result = "Insert Succeed";
+        else
+        	result = "Insert Failed";
+        return result;
+	}
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateCoffee(Coffee cf) throws Exception {
+		String result = "Record entered: "+ cf.toString();
+        System.out.println(result);
+        int r = new CoffeeDao().updateCoffeeSales(cf);
+        if(r > 0) {
+        	return "Update Succeed";
+        }
+        else {
+        	return "Update Failed";
+        }
+	}
+	
+	@POST
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteCoffee(Coffee cf) {
+		String result = null;
+		System.out.println("Record Entered: "+cf.toString());
+		int r = new CoffeeDao().deleteCoffee(cf);
+		if(r > 0)
+			result = "Delete Succeed";
+		else
+			result = "Delete Failed";
+		return result;
 	}
 }
